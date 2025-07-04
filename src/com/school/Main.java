177ff6a4cc0494dee943de40062f6c566545afab
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-07-04 17:55:30 +0530

    feat: Implement encapsulation and basic attendance recording (Part 4)

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -1,4 +1,5 @@
 package com.school;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,5 +26,15 @@ public class Main {
         }
         System.out.println("\nSession 2: Core Domain Modelling Complete.");
         System.out.println("\nSession 3: Constructor and Auto-ID generator completed.");
+
+        List<AttendanceRecord> attendanceLog = new ArrayList<>();
+        attendanceLog.add(new AttendanceRecord(students[0].getStudentId(),courses[0].getCourseId(), "Present"));
+        attendanceLog.add(new AttendanceRecord(students[1].getStudentId(),courses[1].getCourseId(), "Late"));
+        attendanceLog.add(new AttendanceRecord(students[2].getStudentId(),courses[2].getCourseId(), "Absent"));
+        for(AttendanceRecord record: attendanceLog){
+            record.displayDetails();
+        }
+
+        System.out.println("\nSession 4: Data Encapsulation & Attendance Recording Validation");
     }
 }
